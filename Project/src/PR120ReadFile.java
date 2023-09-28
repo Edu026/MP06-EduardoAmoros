fc00704aa514db5b44ca32644999314c605d4b57
Edu026 <73888835+Edu026@users.noreply.github.com>
2023-09-28 23:55:04 +0200

    Final

--- Project/src/PR120ReadFile.java
+++ Project/src/PR120ReadFile.java
@@ -7,13 +7,12 @@ import java.util.List;
 
 public class PR120ReadFile {
     public static void main(String[] args) {
-
-       String rutaAbsoluta = "/home/super/Escriptori/MP06/DAM-JavaPersitenciaFitxers/src/PR120ReadFile.java";
-
+       String rutaAbsoluta = "C:\\Users\\PC\\Documents\\Programacion\\PR1.2-Lectura-i-escriptura\\Project\\src\\PR120ReadFile.java";
+       
         try {
             List<String> linies = Files.readAllLines(Paths.get(rutaAbsoluta, args), StandardCharsets.UTF_8);
             for (int cnt = 0; cnt < linies.size(); cnt = cnt + 1) {
-                System.out.println(linies.get(cnt));
+                System.out.println(cnt + "      " + linies.get(cnt));
             }
         } catch (IOException e) { e.printStackTrace(); }
 
--- Project/src/PR122cat.java
+++ Project/src/PR122cat.java
@@ -1,13 +1,19 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class PR122cat {
-    public static void main(String[] args) {       
-            String basePath = System.getProperty("user.dir");
-            System.out.println(basePath); 
+    public static void main(String[] args) {      
+        
+            System.out.println("\n"+"Path: ");
+            
             Scanner sc = new Scanner(System.in);    
             String checkFilePath = sc.next();
+
             // Comprovar si una ruta és un arxiu
             File f3 = new File(checkFilePath);
             boolean esArxiu = f3.isFile();
@@ -15,7 +21,14 @@ public class PR122cat {
             boolean exArx = f3.exists();
             
             if (esArxiu) {
-                System.out.println("La ruta \"" + f3.getName() + "\" és un arxiu");
+                System.out.println("La ruta \"" + f3.getName() + "\" es un arxiu");
+
+                try {
+                    List<String> linies = Files.readAllLines(Paths.get(checkFilePath, args), StandardCharsets.UTF_8);
+                    for (int cnt = 0; cnt < linies.size(); cnt = cnt + 1) {
+                        System.out.println(linies.get(cnt));
+                    }
+                } catch (IOException e) { e.printStackTrace(); } 
                 
             } else if (esDir){
                     System.out.println("El path no correspon a un arxiu, sinó a una carpeta");
